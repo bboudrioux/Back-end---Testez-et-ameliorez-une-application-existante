5b6548da53eb88ee50fd6ab9cf9d7572b5d34c80
Brian Boudrioux <bboudrioux@gmail.com>
2025-11-28 05:41:44 +0100

    implement login with jwt

--- src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,26 @@
 package com.openclassrooms.etudiant.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
 @Service
 public class JwtService {
 
-    public String generateToken(UserDetails userDetails) {
-        return null; // TODO
-    }
+    private final String SECRET_KEY = "maCleSuperSecretePourJWT1234567890";
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 24; // 24h
 
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes(StandardCharsets.UTF_8))
+                .compact();
+    }
 }
